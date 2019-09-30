37d9ec0b1992e47b2b1e4e71bb7c8de545a0415f
Ritapravo <ritopravosarker@gmail.com>
2019-09-30 14:42:31 +0530

    Algorithms

--- Kadene's algorithm.py
+++
@@ -1,8 +0,0 @@
-def max_subarray_sum(arr,size):
-    m=0 #stores max
-    c=0 #stores current max
-    for i in range(size):
-        c=max(arr[i],c+arr[i])
-        m=max(c,m)
-        print(c)
-    return m
---
+++ Some useful algorithms.py
@@ -0,0 +1,29 @@
+
+#Kadene's algorithm for maximum sub-array sum
+
+def max_subarray_sum(arr,size):
+    m=0 #stores max
+    c=0 #stores current max
+    for i in range(size):
+        c=max(arr[i],c+arr[i])
+        m=max(c,m)
+        print(c)
+    return m
+
+
+
+#Sieve of Eratosthenes for finding prime numbers in a range
+
+def sieve(n):
+    primes=[True]*(n+1)
+    p=2
+    while(p*p<=n):
+        if(primes[p]==True):
+            for i in range(2*p,n+1,p):
+                primes[i]=False
+        p+=1
+    primes[0]=False
+    primes[1]=False
+    for i in range(n+1):
+        if (primes[i]==True):
+            print (i)
